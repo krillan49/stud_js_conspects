198ecfae187386c1906671df7c61228b12b46307
krillan <pushkinidze49@gmail.com>
2023-11-28 16:19:54 +0300

    sgfdsxhfdx

---
+++ c19_chai_mocha.js
@@ -0,0 +1,12 @@
+//                                               Конфигурация
+
+// Настройка конфигурации пишется в начале фаила
+
+// config.includeStack (по умолчанию false) - влияет на то, включается ли трассировка стека в сообщение об ошибке утверждения. По умолчанию false трассировка стека подавляется в сообщении об ошибке.
+chai.config.includeStack = true; // turn on stack trace
+
+// config.showDiff (по умолчанию true) - должен ли showDiff флаг быть включен в выдаваемые AssertionErrors. false всегда будет false; true будет истинным, если утверждение запросило отображение различий.
+chai.config.showDiff = false; // turn off reporter diff display
+
+// config.truncateThreshold (по умолчанию 40) - устанавливает порог длины для фактических и ожидаемых значений в ошибках утверждения. Если этот порог превышен, значение усекается.
+chai.config.truncateThreshold = 0; // disable truncating
